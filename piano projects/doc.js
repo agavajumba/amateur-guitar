3355783f37a248d6e9f6808b396a69f96d1c7407
Agava Jumba <82651225+agavajumba@users.noreply.github.com>
2023-07-02 20:25:26 +0300

    Add files via upload

---
+++ piano projects/doc.js
@@ -0,0 +1,28 @@
+document.querySelectorAll("button")[0].addEventListener("click",function(){
+    let mySound = new Audio("sound1.mp3")
+  mySound.play()
+  })
+  document.querySelectorAll("button")[1].addEventListener("click",function(){
+    let mySound = new Audio("sound2.mp3")
+  mySound.play()
+  })
+  document.querySelectorAll("button")[2].addEventListener("click",function(){
+    let mySound = new Audio("sound3.mp3")
+  mySound.play()
+  })
+  document.querySelectorAll("button")[3].addEventListener("click",function(){
+    let mySound = new Audio("sound4.mp3")
+  mySound.play()
+  })
+  document.querySelectorAll("button")[4].addEventListener("click",function(){
+    let mySound = new Audio("sound5.mp3")
+  mySound.play()
+  })
+  document.querySelectorAll("button")[5].addEventListener("click",function(){
+    let mySound = new Audio("sound6.mp3")
+  mySound.play()
+  })
+  document.querySelectorAll("button")[6].addEventListener("click",function(){
+    let mySound = new Audio("sound7.mp3")
+  mySound.play()
+  })
